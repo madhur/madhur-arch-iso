843e60911acf12070b91530bbe18f7244d48913c
Madhur <ahuja.madhur@gmail.com>
2022-11-06 12:19:30 +0530

    initial commit for arch iso

--- profiledef.sh
+++ profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
+iso_name="archmadhurlinux"
 iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_publisher="Arch Madhur Linux <https://www.madhur.co.in>"
+iso_application="Arch Madhur Linux"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
